af8886797ba0cf9128190ee5a5621ed0c44481ea
peter@NeoGenesis <pieeaterpeter69@gmail.com>
2023-12-20 19:15:05 +0800

    change sqlite3 library use method. pre-compile binary and use static library. different march problem solve later.

--- sshhostdatabase.cpp
+++ sshhostdatabase.cpp
@@ -1,6 +1,18 @@
 #include "sshhostdatabase.h"
 
-sshHostDataBase::sshHostDataBase()
+sshHostDataBase::sshHostDataBase(std::string &sqlitePath):dbPath(sqlitePath)
 {
 
+}
+
+bool sshHostDataBase::connectDatabase()
+{
+  char* connectError=nullptr;
+  auto openStatus=sqlite3_open(this->dbPath.c_str(),&(this->db));
+  if(openStatus)
+    {
+      //open error
+      return false;
+    }
+  return true;
 }
--- sshhostdatabase.h
+++ sshhostdatabase.h
@@ -1,11 +1,26 @@
 #ifndef SSHHOSTDATABASE_H
 #define SSHHOSTDATABASE_H
-
+#include <string>
+#include <memory>
+#include "sqlite3.h"
 
 class sshHostDataBase
 {
 public:
-  sshHostDataBase();
+  sshHostDataBase(std::string& sqlitePath);
+
+
+private:
+  //functions
+  bool connectDatabase();
+  bool initDatabase();
+private:
+  //variables
+  const std::string dbPath;
+  sqlite3* db=nullptr;
+
+
+
 };
 
 #endif // SSHHOSTDATABASE_H
